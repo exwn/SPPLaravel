4b8d1f3c64a1306d7356bf5e275d2ef21678ec62
exwn <ixwanull.4za@outlook.com>
2022-08-21 22:30:53 +0700

    edit: customize style blade

--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,3 +1,6 @@
+<!-- Favicon -->
+<link rel="icon" href="{{ asset('images/logo/favicon_io/favicon.ico') }}">
+
 <!-- Fonts -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
@@ -10,17 +13,21 @@
 <link rel="stylesheet" href="{{ mix('css/bootstrap.css') }}">
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+<!-- Datepicker -->
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"
     integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<!-- Get FilePond and FilePond image preview plugin styles from a CDN -->
-
-
-<link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
-<link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet">
-
+<!-- Datatables -->
+{{--
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css"> --}}
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+<!-- Selectize -->
+{{--
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.13.6/css/selectize.bootstrap2.css"
+    integrity="sha512-sOuOFiIdtJhRUh7Mw1SeKcp9a2LBAktMbmtca5uc/lnD7fXSsM+8h0hq22174hh2tJjSq2ydUBt83VJePsjfjQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
 
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
@@ -29,9 +36,16 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"
     integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.13.6/js/selectize.min.js"
+    integrity="sha512-DBOconMAY06o4R79zeXKKM3h/g5pca647Eabb+6viK4dRpiMOlZFS4gsbukTbHo+ppdKx4yr+/0m2JnpeAIrSw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
 
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
+{{-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> --}}
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 {{-- <script src="https://unpkg.com/filepond@4.30.4/dist/filepond.js"></script>
 
 <script src="https://unpkg.com/jquery-filepond/filepond.jquery.js"></script> --}}
@@ -40,13 +54,16 @@
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script> --}}
 
 <!-- include FilePond library -->
-<script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
+{{-- <script src="https://unpkg.com/filepond/dist/filepond.min.js"></script> --}}
 
 <!-- include FilePond plugins -->
-<script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.js"></script>
+{{-- <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.js"></script>
+--}}
 
 <!-- include FilePond jQuery adapter -->
-<script src="https://unpkg.com/jquery-filepond/filepond.jquery.js"></script>
+{{-- <script src="https://unpkg.com/jquery-filepond/filepond.jquery.js"></script> --}}
+
+{{-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> --}}
 @livewireStyles
 
 {{ $style ?? '' }}
