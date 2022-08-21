a1269feeee14bc99e8ca1a6bb832e2169919fecd
exwn <ixwanull.4za@outlook.com>
2022-08-21 22:19:03 +0700

    add: customizing user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,12 +22,22 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        // $role = $this->faker->numberBetween(1, 3);
+        $role = 3;
+        if ($role == '3') {
+            $kelas = $this->faker->numberBetween(1, 3);
+            $jurusan = $this->faker->numberBetween(1, 4);
+        }
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'no_telp' => $this->faker->numerify('#############'),
+            'role_id' => $role,
+            'kelas_id' => $kelas,
+            'jurusan_id' => $jurusan,
         ];
     }
 }
